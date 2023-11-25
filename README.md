3ac44d66c4462043e893cc8f3154252331ccffc7
JacobLinCool <jacoblincool@gmail.com>
2023-11-26 02:40:05 +0800

    feat: add favicon and screenshot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cite-github
- Generate BibTeX citation for all of your repositories.
+# Cite GitHub
+
+Generate BibTeX citation for all of your repositories.
+
+![Screenshot of Cite GitHub web application](static/screenshot.png)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import "../app.css";
 </script>
 
+<svelte:head>
+	<title>Cite GitHub</title>
+	<meta
+		name="description"
+		content="Generate BibTeX citations for your all GitHub repositories."
+	/>
+	<meta name="keywords" content="GitHub, BibTeX, citation, generator" />
+	<meta name="author" content="Jacob Lin" />
+	<meta name="og:image" content="https://cite.pages.dev/screenshot.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <slot />
